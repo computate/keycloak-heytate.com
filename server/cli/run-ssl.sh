83c48ca7271b9f82a2ac05fad4868233375aaede
ctate <ctate@redhat.com>
2018-08-15 22:11:23 -0600

    Updated the ssl commands.

--- server/cli/run-ssl.sh
+++ server/cli/run-ssl.sh
@@ -1,6 +1,6 @@
-/opt/jboss/keycloak/bin/jboss-cli.sh --commands="embed-server, /core-service=management/security-realm=ApplicationRealm/server-identity=ssl:write-attribute(name=keystore-path, value=keystore/server.jks)"
 /opt/jboss/keycloak/bin/jboss-cli.sh --commands="embed-server, /core-service=management/security-realm=ApplicationRealm/server-identity=ssl:write-attribute(name=alias, value=${KEYSTORE_ALIAS})"
 /opt/jboss/keycloak/bin/jboss-cli.sh --commands="embed-server, /core-service=management/security-realm=ApplicationRealm/server-identity=ssl:write-attribute(name=keystore-password, value=${KEYSTORE_PASSWORD})"
 /opt/jboss/keycloak/bin/jboss-cli.sh --commands="embed-server, /core-service=management/security-realm=ApplicationRealm/server-identity=ssl:write-attribute(name=key-password, value=${KEY_PASSWORD})"
 /opt/jboss/keycloak/bin/jboss-cli.sh --commands="embed-server, /core-service=management/security-realm=ApplicationRealm/server-identity=ssl:undefine-attribute(name=generate-self-signed-certificate-host)"
+/opt/jboss/keycloak/bin/jboss-cli.sh --commands="embed-server, /core-service=management/security-realm=ApplicationRealm/server-identity=ssl:write-attribute(name=keystore-path, value=keystore/server.jks)"
 start-keycloak.sh -b 0.0.0.0
